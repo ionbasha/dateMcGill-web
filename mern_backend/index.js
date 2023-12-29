3ad3edf03f78766fcbb798b1cb9ecece6a80e3b6
Ion Basha <ionbasha15@gmail.com>
2023-12-28 22:46:17 -0800

    Added login functionality: need to fix  404 error when user not found

--- mern_backend/index.js
+++ mern_backend/index.js
@@ -10,9 +10,6 @@ const bcrypt = require('bcrypt')
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.json("Hello to my app!")
-})
 
 app.post('/createaccount', async (req, res) => {
     const client = new MongoClient(uri);
@@ -25,13 +22,12 @@ app.post('/createaccount', async (req, res) => {
         const database = client.db('dateMcGill');
         const userCollection = database.collection('users');
 
-        userExists = await userCollection.findOne({ email })
+        const userExists = await userCollection.findOne({ "email" : email })
 
         if(userExists) {
             return(res.status(409).send("Email has already been registered."))
         }
         else {
-            console.log(res.status)
             const lowerEmail = email.toLowerCase();
             const userData = {
                 firstName: firstName,
@@ -53,11 +49,44 @@ app.post('/createaccount', async (req, res) => {
 
     }
     catch(e) {
-        console.log(e)
+        console.log(e);
+        res.send("There was an error. Please try again.")
     }
+
           
 })
 
+app.post('/login', async (req, res) => {
+    const { emailEntered, passwordEntered } = req.body;
+    const emailEnteredLower = emailEntered.toLowerCase();
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const database = client.db('dateMcGill');
+        const userCollection = database.collection('users');
+
+        const userExists = await userCollection.findOne({ "email" : emailEnteredLower });
+
+        if(userExists) {
+            const passwordsMatch = await bcrypt.compare(passwordEntered, userExists.password);
+
+            if(passwordsMatch) {
+                const jwt_token = jwt.sign(userExists, emailEnteredLower, { expiresIn: 24*60 });
+                res.status(201).json({ jwt_token, email: emailEnteredLower });
+            }
+        }
+        else {
+            res.status(404).send("Invalid email or password.");
+        }
+
+    }
+    catch(e) {
+        res.send("There was an error. Please try again.")
+    }
+
+});
 
 
-app.listen(PORT, () => {console.log("Change made on port 8000")})
+
+app.listen(PORT, () => {console.log("Port 8000")})
--- mern_frontend/src/components/Login.js
+++ mern_frontend/src/components/Login.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { useLayoutEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import '../stylesheets/Login.css'
+import '../stylesheets/LandingPage.css'
+import axios from 'axios'
 
 function Login() {
+
+  const navigate = useNavigate();
+
+  useLayoutEffect(() => {
+    document.body.style.backgroundColor = "#ffe6fe"
+  });
+
+  const [emailEntered, setEmailEntered] = useState('')
+  const [passwordEntered, setPasswordEntered] = useState('')
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    
+    const response = await axios.post('http://localhost:8000/login', { emailEntered, passwordEntered });
+
+    if(response.status === 201) { navigate('/dashboard'); }
+    else { alert("User does not exist. Please ensure that your email and password are correct.") }
+
+  }
+
   return (
-    <div>
-      <p>This is the login page</p>
+    <div className='overlayLogin'>
+      <p>
+          <span className='landingPageTxtB'>date</span>
+          <span className='landingPageTxtR'>McGill</span>
+      </p>
+      <form onSubmit={handleSubmit}>
+        <input 
+        placeholder='Email' 
+        value={emailEntered} 
+        onChange={ e => setEmailEntered(e.target.value) }>
+        </input>
+        <input 
+        placeholder='Password' 
+        type={'password'} 
+        value = {passwordEntered} 
+        onChange = {e => {setPasswordEntered(e.target.value)}}>
+        </input>
+        <input type={'submit'} value='Log In' onSubmit={handleSubmit}></input>
+      </form>
     </div>
   )
 }
